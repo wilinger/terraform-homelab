7fbd92147d04ecda18aec7815659fb0fa275f412
wilinger <>
2022-01-23 12:54:03 -0800

    update readme

--- auth.tf
+++ auth.tf
@@ -59,21 +59,3 @@ resource "vault_generic_endpoint" "clientrole" {
 EOT
 }
 
-# variable "ZEN_PASS" {
-#   description = "User password"
-#   type        = string
-#   sensitive   = true
-# }
-
-# resource "vault_generic_endpoint" "zenuser" {
-#   depends_on           = [vault_auth_backend.userpass]
-#   path                 = "auth/userpass/users/zenuser"
-#   ignore_absent_fields = true
-#   data_json            = <<EOT
-# {
-#   "policies": ["admins"],
-#   "password": "${var.ZEN_PASS}"
-# }
-# EOT
-# }
-
